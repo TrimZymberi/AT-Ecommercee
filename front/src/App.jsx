58e0b7001f34470e56eceef18baef2ed04b5a362
armql <am57839@ubt-uni.net>
2023-08-18 00:25:23 +0200

    Stable Development Workspace
    - Added Front(Vite + React) Stable,
    - Added Back(Laravel 10) stable, database not implemented,
    
    - Added TailwindCSS w/ (HeroIcons, HeadlessUI),
    - Added React-Router-DOM(NOT IMPLEMENTED).

---
+++ front/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1 class="bg-red-500 p-2">Test</h1>
+    </div>
+  )
+}
+
+export default App
