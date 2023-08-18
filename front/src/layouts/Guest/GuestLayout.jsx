51053bced44e2983e592434cdbfb4e977e6d62b9
TrimZymberi <tz58110@ubt-uni.net>
2023-08-18 18:44:34 +0200

    changes

--- front/src/layouts/Guest/GuestLayout.jsx
+++ front/src/layouts/Guest/GuestLayout.jsx
@@ -5,7 +5,7 @@ import Navbar from '../../Components/Navbar'
 export default function GuestLayout() {
     return (
         <div>
-            <Navbar/>
+            <Navbar />
             <Outlet />
         </div>
     )
