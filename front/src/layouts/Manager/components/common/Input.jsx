e014c1265daedf52b36fdbb0ef4352a4c9983ec9
am <arlindmaliqi28@gmail.com>
2024-03-03 19:32:42 +0100

    *

--- front/src/layouts/Manager/components/common/Input.jsx
+++ front/src/layouts/Manager/components/common/Input.jsx
@@ -1,4 +1,14 @@
-export default function Input({ htmlFor, labelName, props, children, value }) {
+export default function Input({
+  htmlFor,
+  labelName,
+  props,
+  children,
+  value,
+  type,
+  name,
+  id,
+  onChange,
+}) {
   return (
     <div>
       <label
@@ -11,6 +21,10 @@ export default function Input({ htmlFor, labelName, props, children, value }) {
         {...props}
         placeholder={labelName}
         value={value}
+        type={type}
+        id={id}
+        name={name}
+        onChange={onChange}
         className="bg-gray-50 border-2 border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
         required=""
       />
